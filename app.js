ab4a775e211747a7b1daeb947ab0b6db829c1460
muhannadsnan <muhannad@cloudmaster.no>
2019-12-05 08:18:22 +0100

    restore3

--- app.js
+++ app.js
@@ -21,18 +21,6 @@ function init() {
     $('#showAddRecord, #hideAddRecord').on('click', toggleAddRecord);
     // pulseAll();
     animateStart();
-    restore();
-}
-function restore(){
-    var x = [
-        new Record('Untitled', 1800, 5601),
-        new Record('الحمد لله', 633, 7174, true),
-        new Record('قل هو الله أحد', 341, 3358),
-        new Record('لا إله إلا الله', 494, 6835),
-        new Record('استغفر الله', 1269, 18052),
-    ];
-    STORE.records = x;
-    saveSTORE();
 }
 
 function initValues(){
